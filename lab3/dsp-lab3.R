e22c2e914318ed567b03e4f1de26d5c86e957602
AJIOB <aleksandr9809@gmail.com>
2018-03-03 19:12:27 +0300

    Original function printing was added

--- lab3/dsp-lab3.R
+++ lab3/dsp-lab3.R
@@ -2,6 +2,19 @@ y <- function(x) {
   return (sin(3 * x) + cos(x))
 }
 
-# testing
-y(1)
-y(2)
+custom_plot <- function(x, y, head_title) {
+  plot.new()
+  plot(x, y, type = "l", xlab = "x", ylab = "y", main = head_title)
+}
+
+N <- 16
+T <- 2 * pi
+
+a <- 0
+b <- a + T
+
+basic_xdots <- seq(a, b, by = 0.01)
+basic_ydots <- y(basic_xdots)
+custom_plot(basic_xdots, basic_ydots, "Basic function")
+
+xdots <- seq(0, N - 1, by = 1)
