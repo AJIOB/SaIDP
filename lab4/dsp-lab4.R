b56260068736f66df40368c305c168dd1249e1d2
AJIOB <aleksandr9809@gmail.com>
2018-04-02 00:15:40 +0300

    Hamming Window signal modification was added

--- lab4/dsp-lab4.R
+++ lab4/dsp-lab4.R
@@ -6,6 +6,7 @@ y <- function(x) {
 
 custom_plot <- function(x, y, head_title) {
   plot(x, y, type = "l", xlab = "x", ylab = "y", main = head_title)
+  abline(h = 0, col = "gray60")
 }
 
 custom_freq_plot <- function(x, y, head_title) {
@@ -38,3 +39,19 @@ custom_plot(basic_xdots, basic_ydots, "Basic function")
 basic_xdots <- custom_seq_interval(a, b, N)
 basic_ydots <- y(basic_xdots)
 custom_plot(basic_xdots, basic_ydots, "Discrete basic function")
+
+hamming_a <- 25/46
+hamming_b <- 21/46
+hidden_hamming_window_ <- function(n, N){
+  hamming_a - hamming_b * cos(2 * pi * n / (N - 1))
+}
+
+# Hamming Window
+hamming_window <- function(dots){
+  N <- length(dots)
+  res <- hidden_hamming_window_(dots, N)
+  res
+}
+
+hamming_ydots <- hamming_window(basic_ydots)
+custom_plot(basic_xdots, hamming_ydots, "Discrete basic function after Hamming Window modification")
